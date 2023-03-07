4e609a75c7fcc1f94178c3da3edc1868f1877304
Önder <ykbgonder@gmail.com>
2023-03-07 12:57:23 +0300

    Todo App
    javascript ile basit Todo Uygulaması

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let gorevListesi = [
+    {"id":1, "gorevAdi":"Gorev"},
+    {"id":2, "gorevAdi":"Gorev"},
+    {"id":3, "gorevAdi":"Gorev"},
+    {"id":4, "gorevAdi":"Gorev"},
+];
+
+
+
